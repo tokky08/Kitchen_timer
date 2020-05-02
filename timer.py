70c2c88b6cd2500a26680fb40146d718325a5976
TOKITA KAZUHITO <tokky.kz08@gmail.com>
2020-05-03 01:03:50 +0900

    実装完了

--- timer.py
+++ timer.py
@@ -4,15 +4,75 @@ import time
 
 
 TIMER_MODE = 'timer'
-
-minute = 2
 second = 59
-start = time.time()
 
-@rumps.clicked(u'残り勤務時間を表示')
-def switchTimer(sender):
-    global display_state
-    display_state = TIMER_MODE
+
+@rumps.clicked(u'1分')
+def selectTimer(sender):
+    global minute
+    minute = 1
+
+    global start
+    start = time.time()
+
+@rumps.clicked(u'3分')
+def selectTimer(sender):
+    global minute
+    minute = 3
+
+    global start
+    start = time.time()
+
+@rumps.clicked(u'5分')
+def selectTimer(sender):
+    global minute
+    minute = 5
+
+    global start
+    start = time.time()
+
+@rumps.clicked(u'10分')
+def selectTimer(sender):
+    global minute
+    minute = 10
+
+    global start
+    start = time.time()
+
+@rumps.clicked(u'15分')
+def selectTimer(sender):
+    global minute
+    minute = 15
+
+    global start
+    start = time.time()
+
+@rumps.clicked(u'20分')
+def selectTimer(sender):
+    global minute
+    minute = 20
+
+    global start
+    start = time.time()
+
+@rumps.clicked(u'25分')
+def selectTimer(sender):
+    global minute
+    minute = 25
+
+    global start
+    start = time.time()
+
+@rumps.clicked(u'30分')
+def selectTimer(sender):
+    global minute
+    minute = 30
+
+    global start
+    start = time.time()
+
+
+
 
 @rumps.timer(1)
 def dispTimer(sender):
@@ -21,13 +81,20 @@ def dispTimer(sender):
     timer_second = str(second - int(time.time() - start) % 60)
 
     remaining_minute = int(time.time() - start) // 60
-        
-    if int(timer_second) < 10:
-        timer_second = "0" + timer_second
-
     timer_minute = str(int(timer_minute) - remaining_minute)
-    app.title = "残り時間：" + timer_minute + ":" + timer_second
-    
+
+    if int(timer_minute) < 0:
+        app.title = "TIME UP"
+        if int(timer_second) == 58:
+            rumps.notification(message="作業をやめてください！！！", title="TIME UP！", subtitle="")
+
+
+    else:
+        if int(timer_second) < 10:
+            timer_second = "0" + timer_second
+
+        app.title = "残り時間：" + timer_minute + ":" + timer_second
+        
     
 
 
